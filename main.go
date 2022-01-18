920570363d9512f2bc36d840b09fb35c5603ef55
tal-seagate <tal.alfi@seagate.com>
2022-01-18 13:54:31 +0000

    adding pesudoDB file and dir

--- main.go
+++ main.go
@@ -29,6 +29,10 @@ func defineRoutes(router *gin.Engine) {
 	router.DELETE("/files/:name", func(c *gin.Context) {
 		fileName := c.Param("name")
 
+		// err := os.Remove("testFile.txt")
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
 		c.String(http.StatusOK, "deleted %s\n", fileName)
 
 	})
